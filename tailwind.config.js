d345877bd5e37ea7d7a017da94bd032548a06289
Rayhan <rayhan@badr.co.id>
2022-02-05 12:28:08 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#F6DE2D",
+				secondary: "#191919",
+			},
+			fontFamily: {
+				primary: ["Lato", "sans-serif"],
+				secondary: ["Playfair Display", "serif"],
+			},
+			container: {
+				center: true,
+				padding: "1rem",
+			},
+		},
+	},
+	plugins: [],
+};
